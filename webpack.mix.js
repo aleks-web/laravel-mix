ef250a9a5627fdccf3a7b11a9350d3b45aa7627b
Alex <dok.go@yandex.ru>
2023-12-18 17:41:45 +0500

    change

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
 let mix = require("laravel-mix");
 
-const site_local_domain = "";
+const site_local_domain = "laravel-mix";
 
-let app_folder_name = "public_html";
+const app_folder_name = "public_html";
+
 
 let app_components_uri = app_folder_name + "/components";
 
@@ -11,9 +12,18 @@ let app_js_uri = app_folder_name + "/assets/js";
 let app_css_uri = app_folder_name + "/assets/css";
 let app_images_uri = app_folder_name + "/assets/images";
 
+
 mix.sass(app_scss_uri + "/main.scss", app_css_uri).disableNotifications();
-// .browserSync({
-//     proxy: site_local_domain,
-//     files: [theme_folder_path + "/assets/js/**/*", theme_folder_path + "/assets/css/**/*", theme_folder_path + "/assets/scss/**/*", theme_folder_path + "/**/*.+(html|php)"],
-//     reloadOnRestart: true,
-// });
+
+
+mix.browserSync({
+    proxy: site_local_domain,
+    files: [
+        app_scss_uri + "/**/*",
+        app_components_uri + "/**/*",
+        app_js_uri + "/**/*",
+        app_images_uri + "/**/*",
+        app_folder_name + "/**/*.+(html|php)"
+    ],
+    reloadOnRestart: true,
+});
